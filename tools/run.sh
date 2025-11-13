6cfe491e8703f1b4b11e1f623c5b43a789cc47ad
Donghee Park <dongheepark@gmail.com>
2025-11-14 07:31:27 +0900

    Add setup scripts

--- tools/run.sh
+++ tools/run.sh
@@ -2,10 +2,17 @@
 
 cd "$(dirname "$0")/.."
 
-sbcl --load "quicklisp/setup.lisp" --load "wde/usability/extend/load-act-r.lisp" --load
-"wde/usability/extend/usability/system_interface.lisp"
+pkill sbcl
 
-#python3 -m venv .venv
-#source .venv/bin/activate
-#pip3 install -r requirements.txt
-#python3 main.py
+# Run background with logging
+echo "Starting ACT-R ..."
+sbcl --load "quicklisp/setup.lisp" --load "wde/usability/extend/load-act-r.lisp" --load "wde/usability/extend/usability/system_interface.lisp" --eval "(usability-system-interface:run-server)" --quit &
+
+sleep 7 
+
+# Run frontend service
+echo "Starting frontend..."
+. .venv/bin/activate
+python3 main.py
+
+pkill sbcl
---
+++ tools/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd "$(dirname "$0")/.."
+
+# Delete all .fasl files for different sbcl versions
+find . -name "*.fasl" -type f -delete
+
+# Delete .venv directory if it exists
+if [ -d ".venv" ]; then
+    rm -rf .venv
+fi
+
+# Install sbcl if not installed
+sudo apt-get install -y sbcl
+
+# Setup for frontend Python environment
+python3 -m venv .venv
+. .venv/bin/activate
+pip3 install -r requirements.txt
